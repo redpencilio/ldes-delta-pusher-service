c3079d0550b2c988b048fbc290e0c2c9b6bfcb3b
karel kremer <karel.kremer@redpencil.io>
2024-10-01 08:11:47 +0200

    remove catch up after restart as auto-healing will handle all of these cases

--- app.ts
+++ app.ts
@@ -1,14 +1,10 @@
+import bodyParser from "body-parser";
+import type { Request, Response } from "express";
 //@ts-ignore
 import { app, errorHandler } from "mu";
-import bodyParser from "body-parser";
 import dispatch from "./config/dispatch";
-import type { Request, Response } from "express";
-import {
-  status,
-  catchUpAfterRestart,
-  storeLastModifiedSynced,
-} from "./catchUpAfterRestart";
-import { Changeset, Quad } from "./types";
+
+import { Changeset } from "./types";
 import { writeInitialState } from "./writeInitialState";
 
 import { cronjob as autoHealing } from "./self-healing/cron";
@@ -24,41 +20,10 @@ app.use(
   })
 );
 
-async function updateLastModifiedSeen(quads: Quad[]) {
-  let highestModified: Date | null = null;
-  quads.forEach((quad) => {
-    if (quad.predicate.value !== "http://purl.org/dc/terms/modified") {
-      return;
-    }
-    const modified = new Date(quad.object.value);
-    if (!highestModified || modified.getTime() > highestModified.getTime()) {
-      highestModified = modified;
-    }
-  });
-  if (highestModified) {
-    await storeLastModifiedSynced(highestModified);
-  }
-}
-
 app.post("/publish", async function (req: Request, res: Response) {
-  if (!status.hasCaughtUpSinceRestart) {
-    // we haven't finished catching up after restarting. We'll see this change by querying the db
-    res
-      .status(202)
-      .send(
-        "Still catching up after restart, we assume this request will be handled during catching up."
-      );
-    return;
-  }
   try {
     const changeSets: Changeset[] = req.body;
     await dispatch(changeSets);
-    await updateLastModifiedSeen(
-      // deletes happen before inserts, so try inserts first, then deletes
-      changeSets[changeSets.length - 1]?.inserts ||
-        changeSets[changeSets.length - 1]?.deletes ||
-        []
-    );
     res.send("Resource added to LDES");
   } catch (e) {
     console.error(e);
@@ -71,7 +36,6 @@ new Promise(async (resolve) => {
     await writeInitialState();
   }
 
-  await catchUpAfterRestart();
   if (AUTO_HEALING == "true") {
     autoHealing.start();
   }
