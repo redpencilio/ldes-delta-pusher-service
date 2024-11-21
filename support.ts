e89e21d44853f916e93f0071b9a3246ab625e518
Nordine Bittich <contact@bittich.be>
2024-11-21 17:28:06 +0100

    bugfixes

--- support.ts
+++ support.ts
@@ -1,6 +1,6 @@
 // @ts-ignore
 import { sparqlEscapeUri, sparqlEscapeString } from "mu";
-import { DATA_FOLDER, LDES_BASE, LDES_FOLDER, LDES_FRAGMENTER } from "./config";
+import { LDES_FOLDER, } from "./config";
 import { Changeset, Quad, Term } from "./types";
 import { addData, getConfigFromEnv } from "@lblod/ldes-producer";
 
@@ -10,8 +10,8 @@ export function toSparqlTerm(thing: Term): string {
 	if (thing.type == "uri") return sparqlEscapeUri(thing.value);
 	// FIXME: without this you probably lose every triples of type boolean & number
 	// a better solution would be to use n3 or something similar
-	else if (thing.datatype?.length) return "\"" + thing.value?.toString() || "" + "\"" + "^^" + this.datatype;
-	else return "\"" + thing.value + "\"";
+	else if (thing.datatype?.length) return "\"" + (thing.value?.toString() || "") + "\"" + "^^" + "<" + thing.datatype + ">";
+	else return "\"" + thing.value?.toString() + "\"";
 }
 
 export function toSparqlTriple(quad: Quad): string {
@@ -22,11 +22,10 @@ export function toSparqlTriple(quad: Quad): string {
 
 export async function moveTriples(changesets: Changeset[], stream = LDES_FOLDER) {
 	let turtleBody = "";
-	console.log("stream", stream);
 	for (const { inserts } of changesets) {
 		if (inserts.length) {
 			inserts.forEach((triple) => {
-				turtleBody += toSparqlTriple(triple) + "\n";
+				turtleBody += toSparqlTriple(triple);
 			});
 
 		}
@@ -35,13 +34,15 @@ export async function moveTriples(changesets: Changeset[], stream = LDES_FOLDER)
 		console.log('nothing to do.');
 		return;
 	}
-	console.log(turtleBody);
-	const fs = require("fs");
-	fs.writeFileSync(DATA_FOLDER + '/debug.ttl', turtleBody, { encoding: 'utf-8' });
+
+
 	await addData(ldesProducerConfig, {
 		contentType: "text/turtle",
 		folder: stream,
 		body: turtleBody,
-		fragmenter: 'time-fragmenter',
 	});
+
+
 }
+
+
